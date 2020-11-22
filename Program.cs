84ec3ded51ccc2b75bbe749b4fbc430c43d94bc9
Name Test <vfrcbvec111@gmail.com>
2020-11-22 22:35:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace WorkflowConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi m8");
+            Console.ReadLine();
+        }
+    }
+}
